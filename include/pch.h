a01b7925711dbd4eccb8732e4e40117e47c68c8b
Emil Motrøen <e.motroen@gmail.com>
2024-12-21 03:49:39 +0100

    Draw triangle on screen

--- include/pch.h
+++ include/pch.h
@@ -1,4 +1,9 @@
-/** Precompiled headers */
+/** 
+ * Precompiled headers.
+ * These are the header files that are used by several source files and
+ * this header file is set to precompile in the CMakeLists file to save
+ * compile time.
+*/
 #ifndef PCH_H
 #define PCH_H
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,23 @@
 #include "main.h"
 
+const char *vertexShaderSource = {
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\n"
+};
+
+const char *fragmentShaderSource = {
+    "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n"
+};
+
 int main() {
     // Initialize GLFW
     if (!glfwInit()) {
@@ -28,6 +46,79 @@ int main() {
         return -1;
     }
 
+    // Vertices of a triangle
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f, // Left
+         0.5f, -0.5f, 0.0f, // Right
+         0.0f,  0.5f, 0.0f  // Top
+    };
+
+    // Create vertexShader
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    // Check if shader compilation was successfull
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cerr << "ERROR: Vertex shader compilation failed." << std::endl;
+    }
+
+    // Create fragment shader
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // Check if shader compilation was successfull
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+    if (!success) {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cerr << "ERROR: Fragment shader compilation failed." << std::endl;
+    }
+
+    // Create shader program and link the shaders
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    // Check if program was created successfully
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cerr << "ERROR: Fragment shader compilation failed." << std::endl;
+    }
+
+    glUseProgram(shaderProgram);
+
+    // Delete shaders as they aren't useful anymore
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+
+
     // Render loop
     while (!glfwWindowShouldClose(window)) {
         // Input
@@ -37,10 +128,19 @@ int main() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         // Check and call events and swap the buffers
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    // Deallocate resources
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
 
     // Terminate and clean up GLFW resources
     glfwTerminate();
